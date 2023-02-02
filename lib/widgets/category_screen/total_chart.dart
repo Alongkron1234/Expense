99fbe77cb5ce3577bbe682c4d42912df3fcff2f8
Alongkorn <alongkron21064@gmail.com>
2023-02-02 11:22:41 +0700

    commit

---
+++ lib/widgets/category_screen/total_chart.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+import '../../models/database_provider.dart';
+
+class TotalChart extends StatefulWidget {
+  const TotalChart({super.key});
+
+  @override
+  State<TotalChart> createState() => _TotalChartState();
+}
+
+class _TotalChartState extends State<TotalChart> {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<DatabaseProvider>(builder: (_, db, __) {
+      var list = db.categories;
+      var total = db.calculateTotalExpenses();
+      return Row(
+        children: [
+          Expanded(
+            flex: 60,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                FittedBox(
+                  alignment: Alignment.center,
+                  fit: BoxFit.scaleDown,
+                  child: Text(
+                    'รายจ่ายรวม: ${NumberFormat.currency(locale: 'en_IN', symbol: '฿').format(total)}',
+                    textScaleFactor: 1.5,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          
+        ],
+      );
+    });
+  }
+}
